3583a4b1aebd779e29083fe6dc9b7520266fc5b4
Roopa Madihally <roopabdr@gmail.com>
2020-04-18 15:25:04 +0530

    Updates

---
+++ precache-manifest.3037f6f214ab04ff5da593c1a5ccefe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44ab1491458a3866d15f88cffaf17dd",
+    "url": "/swathi-the-dancer/index.html"
+  },
+  {
+    "revision": "9dcc87eb77531412cd46",
+    "url": "/swathi-the-dancer/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "759e860465a2ddaf33fd",
+    "url": "/swathi-the-dancer/static/js/2.d60e421d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swathi-the-dancer/static/js/2.d60e421d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dcc87eb77531412cd46",
+    "url": "/swathi-the-dancer/static/js/main.6bb5d4f8.chunk.js"
+  },
+  {
+    "revision": "27c18eb8e02700b1be2b",
+    "url": "/swathi-the-dancer/static/js/runtime-main.16dccd6f.js"
+  }
+]);
